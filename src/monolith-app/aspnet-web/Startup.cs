874ed827209b7c021bba0826ea2bfbc8b43df49f
Zoran Jovanovic <zojovano@microsoft.com>
2019-01-31 22:55:48 +0100

    add

--- src/monolith-app/aspnet-web/Startup.cs
+++ src/monolith-app/aspnet-web/Startup.cs
@@ -49,7 +49,7 @@ namespace aspnet_web
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
