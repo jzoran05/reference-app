d31b5d7f348b0a1c85e8e391bd2636450668a52b
Zoran Jovanovic <zojovano@microsoft.com>
2019-02-08 00:16:55 +0100

    add file

--- src/monolith-app/host/kubernetes/setacrpermission.ps1
+++ src/monolith-app/host/kubernetes/setacrpermission.ps1
@@ -23,3 +23,5 @@ az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
 #    "password": "43867148-5870-40bc-b9c1-b4eb24707a1e",
 #    "tenant": "72f988bf-86f1-41af-91ab-2d7cd011db47"
 #  }
+
+#az aks create --resource-group demo-aks --name csucsaaks-cli --node-count 1 --service-principal bc5b5356-890c-4cb9-82c0-7e5748f2eeb3 --client-secret 43867148-5870-40bc-b9c1-b4eb24707a1e --generate-ssh-keys
